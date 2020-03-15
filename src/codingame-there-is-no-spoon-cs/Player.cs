085e766735facdf3abc4cadf9fe83d3d3a2eb77a
julien <julien.fruteau@gmail.com>
2020-03-15 08:02:41 +0100

    code version ok and submitted

--- src/codingame-there-is-no-spoon-cs/Player.cs
+++ src/codingame-there-is-no-spoon-cs/Player.cs
@@ -6,9 +6,6 @@ namespace codingame_there_is_no_spoon_cs
 {
     class Player
     {
-        // public int width;
-        // public int height;
-        // public List<char[]> grid = new List<char[]>();
         static void Main(string[] args)
         {
             int width = int.Parse(Console.ReadLine()); // the number of cells on the X axis
@@ -18,45 +15,44 @@ namespace codingame_there_is_no_spoon_cs
 
             for (int i = 0; i < height; i++)
             {
-                string line = Console.ReadLine(); // width characters, each either 0 or .
-                grid.AddLine(line);
+                grid.AddLine(Console.ReadLine());
             }
 
             IEnumerable<int[]> iEnumCoordinates = grid.IterCoordinates();
-
-            //To retrieve all the items from the above IEnumerator object, we cannot use foreach loop instead of that we need to invoke MoveNext() Boolean method. 
-            foreach(int[] coordinates in iEnumCoordinates)
+            foreach(int[] coor in iEnumCoordinates)
             {
-                Console.WriteLine(coordinates);
+                string line = null;
+                if (grid.GetCoorValue(coor[0], coor[1]) == "0")
+                {
+                    line = $"{coor[0]} {coor[1]}";
+                }
+                else
+                {
+                    continue;
+                }
+
+                int[] nextRightCoor = grid.NextRightNeighboor(coor[0], coor[1]);
+                if (nextRightCoor != null)
+                {
+                    line += $" {nextRightCoor[0]} {nextRightCoor[1]}";
+                }
+                else
+                {
+                    line += " -1 -1";
+                }
+
+                int[] nextLowerCoor = grid.NextLowerNeighboor(coor[0], coor[1]);
+                if (nextLowerCoor != null)
+                {
+                    line += $" {nextLowerCoor[0]} {nextLowerCoor[1]}";
+                }
+                else
+                {
+                    line += " -1 -1";
+                }
+                Console.WriteLine(line);
             }
-            // Write an action using Console.WriteLine()
-            // To debug: Console.Error.WriteLine("Debug messages...");
-            // for(x, y) in grid.IterCoor():
-            //     # print(x, y, grid(x, y), file = sys.stderr)
-            //     if grid(x, y) == "0":
-            //         line = "{} {}".format(x, y)
-            //     else:
-            //         continue
-            //     if grid.NextRightNeighboor(x, y):
-            //         line += " {} {}".format(*grid.NextRightNeighboor(x, y))
-            //     else:
-            //         line += " -1 -1"
-            //     if grid.NextLowerNeighboor(x, y):
-            //         line += " {} {}".format(*grid.NextLowerNeighboor(x, y))
-            //     else:
-            //         line += " -1 -1"
-            //     print(line)
-
-
-            // Three coordinates: a node, its right neighbor, its bottom neighbor
-            Console.WriteLine("0 0 1 0 0 1");
         }
-
-        // public Grid(int width, int height)
-        // {
-        //     this.width = width;
-        //     this.height = height;
-        // }
     }
 
     public class Grid
@@ -83,17 +79,6 @@ namespace codingame_there_is_no_spoon_cs
             grid.Add(line.ToCharArray());
         }
 
-        // public IEnumerator<int[]> IterCoor()
-        // {
-        //     for (int row = 0; row < height; row++)
-        //     {
-        //         for (int col = 0; col < width; col++)
-        //         {
-        //             yield return new int[] {col, row};
-        //         }
-        //     }
-        // }
-
         public IEnumerable<int[]> IterCoordinates()
         {
             for (int row = 0; row < height; row++)
@@ -105,30 +90,37 @@ namespace codingame_there_is_no_spoon_cs
             }
         }
 
-        // public NextLowerNeighboor(int x, int y)
-        // {
-        //     y += 1;
-        //     while (y < height)
-        //     {
-        //         if 
-        //     }
-        // }
+        public int[] NextLowerNeighboor(int x, int y)
+        {
+            int[] result = null;
+            y++;
+            while (y < this.height)
+            {
+                if (this.GetCoorValue(x, y) == "0")
+                {
+                    result = new int[] { x, y };
+                    break;
+                }
+                y++;
+            }
+            return result;
+        }
 
-        // def NextLowerNeighboor(self, x, y):
-        // y += 1
-        // while y<self.height:
-        //     if grid(x, y) == '0':
-        //         return (x, y)
-        //     y += 1
-        // return None
-
-        // def NextRightNeighboor(self, x, y) :
-        //     x += 1
-        //     while x<self.width:
-        //         if grid(x, y) == '0':
-        //             return (x, y)
-        //         x += 1
-        //     return None
+        public int[] NextRightNeighboor(int x, int y)
+        {
+            int[] result = null;
+            x++;
+            while (x < this.width)
+            {
+                if (this.GetCoorValue(x, y) == "0")
+                {
+                    result = new int[] {x, y};
+                    break;
+                }
+                x++;
+            }
+            return result;
+        }
     }
 
 }
--- test/codingame-there-is-no-spoon-cs.test/PlayerTest.cs
+++ test/codingame-there-is-no-spoon-cs.test/PlayerTest.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using Xunit;
 
 namespace codingame_there_is_no_spoon_cs.test
@@ -6,18 +7,79 @@ namespace codingame_there_is_no_spoon_cs.test
     public class PlayerTest
     {
         [Fact]
-        public void Test1()
+        public void GetCoorValueTest()
         {
             // Arrange
             Grid grid = new Grid(4, 2);
-
             // Act
             grid.AddLine("0000");
             grid.AddLine("0..0");
 
             // Assert
-            string coorVal = grid.GetCoorValue(0,0);
-            Assert.Equal("0", coorVal.ToString());
+            // string coorVal = grid.GetCoorValue(0,0);
+            Assert.Equal("0", grid.GetCoorValue(0, 0));
+            Assert.Equal(".", grid.GetCoorValue(2, 1));
+            Console.WriteLine("Test GetCoorValue : OK");
+        }
+
+        [Fact]
+        public void IterCoordinatesTest()
+        {
+            Console.WriteLine("Test IterCoordinates : ");
+            // Arrange
+            Grid grid = new Grid(4, 2);
+            // Act
+            grid.AddLine("0000");
+            grid.AddLine("0..0");
+
+            IEnumerable<int[]> iEnumCoordinates = grid.IterCoordinates();
+            //To retrieve all the items from the above IEnumerator object, we cannot use foreach loop instead of that we need to invoke MoveNext() Boolean method.
+            foreach (int[] coor in iEnumCoordinates)
+            {
+                Console.WriteLine($"coordinates : ({coor[0]}, {coor[1]}) - Value : {grid.GetCoorValue(coor[0], coor[1])}");
+            }
+        }
+
+        [Fact]
+        public void SyntaxTest()
+        {
+            Console.WriteLine("Test Syntax :");
+            string line = null;
+            Assert.Equal(null, line);
+
+            line = "toto";
+            Console.WriteLine(line);
+        }
+
+        [Fact]
+        public void NextRightNeighboorTest()
+        {
+            // Arrange
+            Grid grid = new Grid(4, 1);
+            // Act
+            grid.AddLine("0.0.");
+
+            // Assert
+            // string coorVal = grid.GetCoorValue(0,0);
+            Assert.Equal(new int[]{2, 0}, grid.NextRightNeighboor(0, 0));
+            Assert.Equal(null, grid.NextRightNeighboor(2, 0));
+            Console.WriteLine("Test NextRightNeighboor : OK");
+        }
+
+        [Fact]
+        public void NextLowerNeighboorTest()
+        {
+            // Arrange
+            Grid grid = new Grid(4, 3);
+            // Act
+            grid.AddLine("0.0.");
+            grid.AddLine("000.");
+            grid.AddLine("0..0");
+            // Assert
+            Assert.Equal(new int[] { 0, 1 }, grid.NextLowerNeighboor(0, 0));
+            Assert.Equal(null, grid.NextLowerNeighboor(1, 1));
+            Assert.Equal(new int[] { 2, 1 }, grid.NextLowerNeighboor(2, 0));
+            Console.WriteLine("Test NextLowerNeighboor : OK");
         }
     }
 }
